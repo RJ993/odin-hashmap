d44d643ddabd81a1d13cebaf57865adbf386a6c0
RJ993 <196080894+RJ993@users.noreply.github.com>
2025-06-14 15:32:23 -0400

    Add rubocop

--- lib/hashset_class.rb
+++ lib/hashset_class.rb
@@ -2,7 +2,6 @@ require_relative 'linked_list_class/l_l_class'
 require_relative 'linked_list_class/l_l_node'
 
 class HashSet
-  attr_reader :length_of_map
   attr_accessor :buckets, :capacity, :length_of_map
 
   def initialize
@@ -13,29 +12,30 @@ class HashSet
   end
 
   def hash(key)
-   hash_code = 0
-   prime_number = 31
-      
-   key.each_char { |char| hash_code = prime_number * hash_code + char.ord }
-   @buckets = @buckets + Array.new(@capacity, nil) 
-   if @length_of_map > @load_factor * @capacity
-     @buckets = @buckets + Array.new(@capacity, nil)
-     @capacity *= 2
-   end
-   hash_code = hash_code % @capacity
+    hash_code = 0
+    prime_number = 31
+
+    key.each_char { |char| hash_code = prime_number * hash_code + char.ord }
+    @buckets += Array.new(@capacity, nil)
+    if @length_of_map > @load_factor * @capacity
+      @buckets += Array.new(@capacity, nil)
+      @capacity *= 2
+    end
+    hash_code %= @capacity
   end
 
   def set(key)
     hash_code = hash(key)
     bucket = @buckets[hash_code]
-    if bucket == nil
+    if bucket.nil?
       @buckets.delete_at(hash_code)
       @buckets.insert(hash_code, LinkedList.new)
       bucket = @buckets[hash_code]
       bucket.append(key)
       @length_of_map += 1
     else
-     return if bucket.contains?(key) == true
+      return if bucket.contains?(key) == true
+
       @length_of_map += 1
       bucket.append(key)
     end
@@ -44,56 +44,59 @@ class HashSet
   def get(key)
     hash_code = hash(key)
     bucket = @buckets[hash_code]
-    return nil if bucket == nil
+    return nil if bucket.nil?
+
     pointer = bucket.head_node
     value = pointer.node_value
     until value == key
       pointer = pointer.next_value
-      return if pointer == nil
+      return if pointer.nil?
+
       value = pointer.node_value
     end
-    return key
+    key
   end
 
   def has?(key)
     hash_code = hash(key)
     bucket = @buckets[hash_code]
-    if bucket != nil
+    return if bucket.nil?
+
     pointer = bucket.head_node
     value = pointer.node_value
-      until value == key
-        pointer = pointer.next_value
-        return if pointer == nil
-        value = pointer.node_value
-      end
-      if pointer.node_value == key
-        return true
-      else
-        return false
-      end
+    until value == key
+      pointer = pointer.next_value
+      return if pointer.nil?
+
+      value = pointer.node_value
     end
+    return true if pointer.node_value == key
+
+    false
   end
 
   def remove(key)
     hash_code = hash(key)
     bucket = @buckets[hash_code]
-    return nil if bucket == nil
+    return nil if bucket.nil?
+
     pointer = bucket.head_node
     value = pointer.node_value
     list_index = 0
     until value == key
       pointer = pointer.next_value
-      return if pointer == nil
+      return if pointer.nil?
+
       list_index += 1
       value = pointer.node_value
     end
     bucket.remove_at(list_index)
     @length_of_map -= 1
-    @buckets[hash_code] = nil if bucket.head_node == nil
+    @buckets[hash_code] = nil if bucket.head_node.nil?
   end
 
   def length
-    return self.length_of_map
+    length_of_map
   end
 
   def clear
@@ -105,16 +108,16 @@ class HashSet
   def entries
     array = []
     @buckets.each do |bucket|
-      if bucket != nil
-        pointer = bucket.head_node
-        value = pointer.node_value
-        (bucket.size).times do
-          array.push(value)
-          pointer = pointer.next_value
-          value = pointer.node_value if pointer != nil
-        end
+      next if bucket.nil?
+
+      pointer = bucket.head_node
+      value = pointer.node_value
+      bucket.size.times do
+        array.push(value)
+        pointer = pointer.next_value
+        value = pointer.node_value unless pointer.nil?
       end
     end
-    return array
+    array
   end
 end
